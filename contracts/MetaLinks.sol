74d85bbbd10bed9421eae7f411df455d05195123
Efen Stakes <efenstakes101@gmail.com>
2022-03-24 14:53:58 +0300

    add metslinkd contract state variables

---
+++ contracts/MetaLinks.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts@4.4.2/access/Ownable.sol";
+
+
+contract MetaLinks is Ownable {
+
+    // total number of avatars so far
+    uint256 public totalAvatars;
+
+    // total number of links so far
+    uint256 public totalMetaLinks;
+
+    // map a metalink id to their addresses
+    mapping( address => uint256 ) public addressesToMID;
+
+    // map an id to avatar data
+    mapping( uint256 => Avatar ) public midsToAvatars;
+    
+    // map an metalink id to links
+    mapping( uint256 => MetaLink ) public midsToMetaLinks;
+
+
+
+
+
+}
