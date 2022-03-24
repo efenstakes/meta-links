31d2f677da7c2539688647baf66b1e4b0b3c558d
Efen Stakes <efenstakes101@gmail.com>
2022-03-24 16:40:22 +0300

    add memory to array return

--- contracts/MetaLinks.sol
+++ contracts/MetaLinks.sol
@@ -218,7 +218,7 @@ contract MetaLinks is Ownable {
 
     
     // get an avatar given its id
-    function getAvatar(uint256 id) public view returns(string memory, string memory, string memory, string memory, uint[] links) {
+    function getAvatar(uint256 id) public view returns(string memory, string memory, string memory, string memory, uint[] memory links) {
         Avatar memory avatar = midsToAvatars[id];
 
         return ( avatar.name, avatar.aka, avatar.bio, avatar.avatar, avatar.links );
